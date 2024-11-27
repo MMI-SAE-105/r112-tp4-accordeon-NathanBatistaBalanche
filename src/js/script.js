55fb06821cbb38d2332b5b0a21fec7718da6600d
NathanBatistaBalanche <nathan.batista_balanche@edu.univ-fcomte.fr>
2024-11-27 11:12:17 +0100

    Adaptation du clic sur un des summary

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 // *** ACCORDEON ***
+
+const acco = document.querySelector(".accordeon");
+
+accordeon.querySelectorAll("summary").forEach((summary) => {
+    summary.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        const details = event.target.closest("details");
+
+        accordeon.querySelectorAll("details").forEach((det) => {
+            if (det !== details) {
+                det.open = false;
+            }
+        });
+
+        details.open = !details.open;
+    });
+});
